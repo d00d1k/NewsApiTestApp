f12bbad7fe591822a609b5e907c34ce46e5ac909
d00d1k <nknik204@icloud.com>
2020-08-23 17:11:57 +0300

    Cleaning project

--- Pods/Target Support Files/Pods-NewsApiTestApp-NewsApiTestAppUITests/Pods-NewsApiTestApp-NewsApiTestAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NewsApiTestApp_NewsApiTestAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_NewsApiTestApp_NewsApiTestAppUITests
-@end
--- Pods/Target Support Files/Pods-NewsApiTestApp/Pods-NewsApiTestApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NewsApiTestApp : NSObject
-@end
-@implementation PodsDummy_Pods_NewsApiTestApp
-@end
